355932b8a764337f35d7fc8112dcd8d353d2e345
TinhBa <khuatbatinh18092006@gmail.com>
2026-09-22 10:46:29 +0700

    Time: 39 ms (5.03%) | Memory: 110.8 MB (8.19%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int value : map.values()) {
+            if (value > 1) return true;
+        }
+        
+        return false;
+    }
+}
